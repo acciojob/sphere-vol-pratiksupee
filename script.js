2b0451a95fe2414f481b6deba536ce44c47b2d64
pratiksupee <135314162+pratiksupee@users.noreply.github.com>
2026-02-09 19:35:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radiusInput = document.getElementById("radius").value;
+    const radius = parseFloat(radiusInput);
+    let volume;
+    if (isNaN(radius) || radius < 0) {
+        volume = NaN;
+    } else {
+        volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+        volume = volume.toFixed(4);
+    }
+    document.getElementById("volume").value = volume;
+    return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
